bca09c0314c28b7c059c96e2317e465a417684df
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-07-16 10:06:07 -0300

    Atualização

--- main.js
+++ main.js
@@ -2,17 +2,27 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector(".textoResultado");
 
-{
-    enunciado: "Em sua aula de Biologia, você se dá conta de que as mudanças climáticas que estão ocorrendo no planeta são consequências do aquecimento global...",
-    alternativas: [
-        {
-            texto: "Isso é perturbador!",
-            afirmacao: "No futuro, com o avanço da tecnologia, deverão surgir profissões que ajudem a diminuir o impacto do homem sobre a natureza." 
-        },
-        {
-            texto: "Olha aí uma oportunidade!",
-            afirmacao: "Então esse é o momento de se capacitar em tecnologias promovendo a sua inclusão digital."
-        }
-    }
+const perguntas = [
+    {
+        enunciado: "Pergunta1",
+        alternativas: ["Alternativa1", "Alternativa2"]
+    },
+    {
+        enunciado: "Pergunta2",
+        alternativas: ["Alternativa1", "Alternativa2"]
+    },
+    {
+        enunciado: "Pergunta3",
+        alternativas: ["Alternativa1", "Alternativa2"]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta();
