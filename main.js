6e7c899d574c1d33c745b989d575bc721848091b
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-07-18 15:34:32 -0300

    atualizar

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".resultado-1");
+const caixaResultado2 = document.querySelector(".resultado-2");
 const textoResultado = document.querySelector(".textoResultado");
+const botao1 = document.querySelector(".caixa-alternativas .botao_1");
+const botao2 = document.querySelector(".caixa-alternativas .botao_2");
+console.log(botao1);
 
-const perguntas = [
-    {
-        enunciado: "Pergunta1",
-        alternativas: ["Alternativa1", "Alternativa2"]
-    },
-    {
-        enunciado: "Pergunta2",
-        alternativas: ["Alternativa1", "Alternativa2"]
-    },
-    {
-        enunciado: "Pergunta3",
-        alternativas: ["Alternativa1", "Alternativa2"]
-    },
-];
+/*Interceptar o click do botão*/
 
-let atual = 0;
-let perguntaAtual;
-function mostraPergunta(){
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-}
-mostraPergunta();
+botao1.addEventListener("click", function(){
+    console.dir(caixaResultado); /*Mostra o elemento html em formato de objeto*/
+    caixaResultado.classList.add("mostrar-caixa-resultado");
+    
+})
+
+botao2.addEventListener("click", function(){
+    console.dir(caixaResultado); /*Mostra o elemento html em formato de objeto*/
+    caixaResultado2.classList.add("mostrar-caixa-resultado");
+    
+})
+
+
+
